ddcec4e0600336315aa6bb103dd8f484e5b9fcae
sasikumarmofs1@gmail.com <ksaha@mesos101.itp.objectfrontier.com>
2016-11-02 19:11:15 -0400

    2_11_commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,7 @@
 
+import com.google.common.collect.ImmutableMap;
+
+
 //// Basic Spark Library
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -37,47 +40,81 @@ object Main {
 
 				val sc =  new SparkContext(conf)
 
-				val emr_labscorepopulated_rdd = sc.cassandraTable("emrbots_data", "emr_labscorepopulated")
-				
-				println("/////////////////////////////////////// " + emr_labscorepopulated_rdd.count())
-				
-				// Total number of records in this table = 107535277
-				emr_labscorepopulated_rdd.take(200000).foreach(println)
+				// Spark-SQL Context
+				val sqlCtx = new SQLContext(sc)
 
 				/**
-		  // Spark-SQL Context
-				val sqlCtx = new SQLContext(sc)
+				val emr_labscorepopulated_rdd = sc.cassandraTable("emrbots_data", "emr_labscorepopulated")
+
+				println("/////////////////////////////////////// " + emr_labscorepopulated_rdd.count())
+
+				// Total number of records in this table = 107535277
+				emr_labscorepopulated_rdd.take(200000).foreach(println)
+				 **/
 
 				val df = sqlCtx.read
 				.format("org.apache.spark.sql.cassandra")
-				.options(Map( "table" -> "data", "keyspace" -> "iris"))
+				.options(Map( "table" -> "protocol_subject_109", "keyspace" -> "remote_monitoring_uci"))
 				.load()
 
-				def encodeLabel=udf((flower: String) => {
-					flower match {
-					case "Iris-setosa" => 0.0
-					case "Iris-versicolor" => 1.0
-					case "Iris-virginica" => 2.0
-					//case _ => 0.0
-					}})
+				// Remove Time_Stamp Column here as it is non-important attribute
+				val df_minus_timestamp = df.drop("time_stamp")
 
-				def toVec4 =udf((sepal_length: Double, sepal_width: Double,petal_length: Double,petal_width: Double) => {
-					Vectors.dense(sepal_length, sepal_width, petal_length,petal_width)
-				})
+				/**
+				 * Imputation Code has to go here
+				 */
 
-				val df_mod = df.withColumn(
+				// HACK - Replaced value NA with 141
+				//val df1 = df_minus_timestamp.distinct()
+				
+				
+				val df1 = df_minus_timestamp.na.replace("heart_rate", ImmutableMap.of("NA", "0"));
+        val df_imputed = df1.filter("heart_rate != 0")
+		 
+        println("&&&&&&&&&&&&&&&&&&&&&&&&&  =  " + df_imputed.count())
+		//df_imputed.
+		
+		def encodeLabel=udf((activity_id: Double) => {
+			activity_id match {
+			 case 1 => 0.0 
+			case 2 => 1.0
+			case 3 => 2.0
+			case 4 => 3.0
+			case 5 => 4.0
+			case 6 => 5.0
+			case 7 => 6.0
+			case 9 => 7.0
+			case 10 => 8.0
+			case 11 => 9.0
+			case 12 => 10.0
+			case 13 => 11.0
+			case 16 => 12.0
+			case 17 => 13.0
+			case 18 => 14.0
+			case 19 => 15.0
+			case 20 => 16.0
+			case 24 => 17.0
+			//case 0 => 18.0
+			case _ => 18.0
+			}})
+
+				def toVec4 =udf((heart_rate: Double,
+						IMU_hand_temperature: Double) => {
+							Vectors.dense(heart_rate,
+									IMU_hand_temperature)})
+
+
+				val df_mod = df_imputed.withColumn(
 						"features",
 						toVec4(
-								df("sepal_length"),
-								df("sepal_width"),
-								df("petal_length"),
-								df("petal_width")
-								)
-						).withColumn("label", encodeLabel(df("flower"))).select("features", "label")
+								df_imputed("heart_rate"),
+								df_imputed("IMU_hand_temperature"))
+						).withColumn("label", encodeLabel(df_imputed("activity_id")))
+				.select("features", "label")
 
-						df_mod.show()
+				df_mod.show(10)   
 
-				val splits = df_mod.randomSplit(Array(0.01, 0.99))
+				val splits = df_mod.randomSplit(Array(0.7, 0.3))
 
 				val (training_split, test_split) = (splits(0), splits(1))
 
@@ -93,38 +130,38 @@ object Main {
 						row.getAs[org.apache.spark.mllib.linalg.Vector]("features")
 						))
 
-				trainingData.cache()
-				testData.cache()
+			//	trainingData.cache()
+			//	testData.cache()
 
 				// Train a DecisionTree model.
 				//  Empty categoricalFeaturesInfo indicates all features are continuous.
-				val numClasses = 3
+				val numClasses = 19
 				val categoricalFeaturesInfo = Map[Int, Int]()
 				val impurity = "entropy"
 				val maxDepth = 3
 				val maxBins = 5
 
 				// Random Forest Stuff
-				val numTrees = 1 // Use more in practice.
-        val featureSubsetStrategy = "auto" // Let the algorithm choose.
+				val numTrees = 100 // Use more in practice.
+				val featureSubsetStrategy = "auto" // Let the algorithm choose.
 
-       val model = RandomForest.trainClassifier(trainingData, numClasses, 
-                              categoricalFeaturesInfo,numTrees, 
-                               featureSubsetStrategy, impurity, maxDepth, maxBins)
+				 val model = RandomForest.trainClassifier(trainingData, numClasses, 
+				 categoricalFeaturesInfo,numTrees, 
+				 featureSubsetStrategy, impurity, maxDepth, maxBins)
 
-			//	val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-				//		impurity, maxDepth, maxBins)
+				// val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+						// impurity, maxDepth, maxBins)
 
-         // Train a GradientBoostedTrees model.
-// The defaultParams for Classification use LogLoss by default.
-val boostingStrategy = BoostingStrategy.defaultParams("Classification")
-boostingStrategy.numIterations = 3 // Note: Use more iterations in practice.
-boostingStrategy.treeStrategy.numClasses = 3
-boostingStrategy.treeStrategy.maxDepth = 3
-// Empty categoricalFeaturesInfo indicates all features are continuous.
-boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
+				// Train a GradientBoostedTrees model.
+				// The defaultParams for Classification use LogLoss by default.
+				val boostingStrategy = BoostingStrategy.defaultParams("Classification")
+				boostingStrategy.numIterations = 3 // Note: Use more iterations in practice.
+				boostingStrategy.treeStrategy.numClasses = 3
+				boostingStrategy.treeStrategy.maxDepth = 3
+				// Empty categoricalFeaturesInfo indicates all features are continuous.
+				boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
 
-// val model = GradientBoostedTrees.train(trainingData, boostingStrategy)
+				// val model = GradientBoostedTrees.train(trainingData, boostingStrategy)
 
 				val labelAndPreds = testData.map { point =>
 				val prediction = model.predict(point.features)
@@ -132,16 +169,17 @@ boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
 		}
 
 		val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
-				println("Test Error //////////////////////////////////////////////= " + testErr)
-				println("Learned classification tree model/////////////////////////:\n" + model.toDebugString)
+		println("Prediction Accuracy ////////////////////////////////////////////// = " + (100-testErr*100) + "%")
+			//	println("Test Error //////////////////////////////////////////////= " + testErr)
+				//println("Learned classification tree model/////////////////////////:\n" + model.toDebugString)
 
 				//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				 */
+
 				// Working 107535277
 				// emr_labscorepopulated_data.take(50000).foreach(println)
 
 				sc.stop()
-	}
+		}
 
-}
+	}
 
